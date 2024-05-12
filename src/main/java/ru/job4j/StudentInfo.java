4a2b9c1c88ad10f44cf0635b023a2ff72394cfd2
Viktor Korchagin <viktor.korchagin.corp@gmail.com>
2024-05-12 16:30:18 +0500

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Viktor Korchagin");
+        System.out.println("24.10.1995");
+    }
+}
