ccfff4059257b1d33bc488e71e74d699cb15dfe3
Viktor Korchagin <viktor.korchagin.corp@gmail.com>
2024-05-15 02:36:57 +0500

    4.6.1. Switch [#334070]

---
+++ src/main/java/ru/job4j/condition/SwitchWeek.java
@@ -0,0 +1,35 @@
+package ru.job4j.condition;
+
+public class SwitchWeek {
+    public static String nameOfDay(int day) {
+        String name;
+
+        switch (day) {
+            case 1:
+                name = "Понедельник";
+                break;
+            case 2:
+                name = "Вторник";
+                break;
+            case 3:
+                name = "Среда";
+                break;
+            case 4:
+                name = "Четверг";
+                break;
+            case 5:
+                name = "Пятница";
+                break;
+            case 6:
+                name = "Суббота";
+                break;
+            case 7:
+                name = "Воскренсенье";
+                break;
+            default:
+                name = "Ошибка";
+        }
+
+        return name;
+    }
+}
