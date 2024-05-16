fde1d457a7e3d335ae908d1af17b077add49d850
Viktor Korchagin <viktor.korchagin.corp@gmail.com>
2024-05-17 00:59:09 +0500

    6.6.2. Двухмерный массив. Циклы. [#306725]

---
+++ src/main/java/ru/job4j/array/MatrixSum.java
@@ -0,0 +1,15 @@
+package ru.job4j.array;
+
+public class MatrixSum {
+    public static int sum(int[][] array) {
+        int result = 0;
+
+        for (int[] nestedArray : array) {
+            for (int number : nestedArray) {
+                result += number;
+            }
+        }
+
+        return result;
+    }
+}
