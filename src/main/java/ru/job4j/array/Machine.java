a61cec1662fa1a538bdfaa2d30d073700985c05d
Viktor Korchagin <viktor.korchagin.corp@gmail.com>
2024-05-17 03:20:07 +0500

    6.8.1. Жадный алгоритм. Сдача в кофе машине [#34741]

---
+++ src/main/java/ru/job4j/array/Machine.java
@@ -0,0 +1,21 @@
+package ru.job4j.array;
+
+import java.util.Arrays;
+
+public class Machine {
+    public static int[] change(int money, int price) {
+        int[] coins = new int[]{10, 5, 2, 1};
+        int[] result = new int[100];
+        int change = money - price;
+        int size = 0;
+
+        for (int coin : coins) {
+            while (change >= coin) {
+                change -= coin;
+                result[size] = coin;
+                size++;
+            }
+        }
+        return Arrays.copyOf(result, size);
+    }
+}
