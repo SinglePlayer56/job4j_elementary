e73ab21c912b3fd8197f5377ad139a32aa444995
Viktor Korchagin <viktor.korchagin.corp@gmail.com>
2024-05-17 01:42:08 +0500

    6.7.4. Выигрышные комбинации в сокобан [#53859]

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -36,4 +36,18 @@ public class MatrixCheck {
 
         return result;
     }
+
+    public static boolean isWin(char[][] board) {
+        boolean result = false;
+        char[] array = extractDiagonal(board);
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 'X' && (monoHorizontal(board, i) || monoVertical(board, i))) {
+                result = true;
+                break;
+            }
+        }
+
+        return result;
+    }
 }
