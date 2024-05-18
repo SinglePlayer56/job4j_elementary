8d71a41e668842c0fc88da5d096e3842c35ae770
Viktor Korchagin <viktor.korchagin.corp@gmail.com>
2024-05-18 12:27:26 +0500

    6.7.3. Массив из диагонали матрицы.  [#214128 #496568] (Убрал отступы)

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -25,11 +25,9 @@ public class MatrixCheck {
 
     public static char[] extractDiagonal(char[][] board) {
         char[] result = new char[board.length];
-
         for (int i = 0; i < result.length; i++) {
             result[i] = board[i][i];
         }
-
         return result;
     }
 
