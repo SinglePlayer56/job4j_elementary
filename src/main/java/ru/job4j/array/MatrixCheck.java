b6f773fbd4253a779000fcb2d0033d53f042eee8
Viktor Korchagin <viktor.korchagin.corp@gmail.com>
2024-05-18 12:26:41 +0500

    Empty

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -25,9 +25,11 @@ public class MatrixCheck {
 
     public static char[] extractDiagonal(char[][] board) {
         char[] result = new char[board.length];
+
         for (int i = 0; i < result.length; i++) {
             result[i] = board[i][i];
         }
+
         return result;
     }
 
