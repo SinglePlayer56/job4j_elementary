4b63e9448d230bde202694eaafdea9f3c657c8d4
Viktor Korchagin <viktor.korchagin.corp@gmail.com>
2024-05-27 10:58:34 +0500

    2.0. Рефакторинг - Расстояние между точками. [#122587]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -8,48 +8,40 @@ class PointTest {
     @Test
     void when00to20then2() {
         double expected = 2;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 0);
         double delta = 0.01;
-        double output = Point.distance(x1, y1, x2, y2);
+        double output = a.distance(b);
         assertThat(output).isEqualTo(expected, withPrecision(delta));
     }
 
     @Test
     void when01toMinus10then1Dot41() {
         double expected = 1.41;
-        int x1 = 0;
-        int y1 = 1;
-        int x2 = -1;
-        int y2 = 0;
+        Point a = new Point(0, 1);
+        Point b = new Point(-1, 0);
         double delta = 0.01;
-        double output = Point.distance(x1, y1, x2, y2);
+        double output = a.distance(b);
         assertThat(output).isEqualTo(expected, withPrecision(delta));
     }
 
     @Test
     void when11to2Minus3then4Dot12() {
         double expected = 4.12;
-        int x1 = 1;
-        int y1 = 1;
-        int x2 = 2;
-        int y2 = -3;
+        Point a = new Point(1, 1);
+        Point b = new Point(2, -3);
         double delta = 0.01;
-        double output = Point.distance(x1, y1, x2, y2);
+        double output = a.distance(b);
         assertThat(output).isEqualTo(expected, withPrecision(delta));
     }
 
     @Test
     void when30to03then4Dot24() {
         double expected = 4.24;
-        int x1 = 3;
-        int y1 = 0;
-        int x2 = 0;
-        int y2 = 3;
+        Point a = new Point(3, 0);
+        Point b = new Point(0, 3);
         double delta = 0.01;
-        double output = Point.distance(x1, y1, x2, y2);
+        double output = a.distance(b);
         assertThat(output).isEqualTo(expected, withPrecision(delta));
     }
 }
