a4e1c3e5dc59fb35a0a8fd8b104d53114b03c888
Viktor Korchagin <viktor.korchagin.corp@gmail.com>
2024-10-27 21:30:11 +0500

    2.0. FileOutputStream [#252488]

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -1,9 +1,17 @@
 package ru.job4j;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class Multiple {
     public static void main(String[] args) {
-        for (int i = 1; i < 10; i++) {
-            System.out.println("1 * " + i + " = " + i);
+        try (FileOutputStream output = new FileOutputStream("../../maven-projects/job4j_design/data/dataresult.txt")) {
+            for (int i = 1; i < 10; i++) {
+                output.write("1 * %d = %d".formatted(i, i).getBytes());
+                output.write(System.lineSeparator().getBytes());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
